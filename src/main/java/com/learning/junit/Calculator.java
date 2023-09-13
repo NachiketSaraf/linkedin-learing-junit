0a8f609ffaede668de4a62218c9555db5b81674e
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-14 01:29:18 +0530

    Calculator

--- src/main/java/com/learning/junit/Calculator.java
+++ src/main/java/com/learning/junit/Calculator.java
@@ -4,4 +4,11 @@ public class Calculator {
     public int sum(int x , int y){
         return x+y;
     }
+    public int subtract(int x,int y){
+        return x-y;
+    }
+
+    public boolean isEven(int i) {
+        return i%2 == 0;
+    }
 }
--- src/test/java/com/learning/junit/CalculatorTest.java
+++ src/test/java/com/learning/junit/CalculatorTest.java
@@ -13,4 +13,16 @@ public class CalculatorTest extends TestCase {
         int actual = calculator.sum(1,2);
         assertEquals(expected,actual);
     }
+
+    public void testSubtract() {
+        int expected = 3;
+        int actual = calculator.subtract(5,2);
+        assertEquals(expected,actual);
+    }
+    public void testIsEven(){
+
+        boolean actual = calculator.isEven(4);
+
+        assertTrue(actual);
+    }
 }
